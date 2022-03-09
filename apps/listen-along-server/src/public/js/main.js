39035a73938f6329e8efc49616068e672c22d8f2
Maxime PRODHOMME <m.prodhomme@student.helmo.be>
2022-03-09 23:59:42 +0100

    Recherche d'un titre fonctionelle (+ cas limites) Rechercher un moyen d'afficher l'image de la Track as meta ?

--- apps/listen-along-server/src/public/js/main.js
+++ apps/listen-along-server/src/public/js/main.js
@@ -1,27 +1,40 @@
 const transferPlayback = async (device_id, token) => {
-  await fetch(
-    "https://api.spotify.com/v1/me/player",
-    {
-      method: 'PUT',
-      headers: {
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "device_ids": [device_id]
-      })
-    }
-  )
+  await fetch('https://api.spotify.com/v1/me/player', {
+    method: 'PUT',
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      device_ids: [device_id],
+    }),
+  });
 };
 
 const play = async (spotify_uri, device_id, token) => {
+  await fetch(
+    `https://api.spotify.com/v1/me/player/play?device_id=${device_id}`,
+    {
+      method: 'PUT',
+      body: JSON.stringify({ uris: [spotify_uri] }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
 
-  await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
-    method: 'PUT',
-    body: JSON.stringify({ uris: [spotify_uri] }),
+const searchItem = async (query, token) => {
+  let urlBase = 'https://api.spotify.com/v1/search?';
+  let url = new URLSearchParams();
+  url.append('type', 'track');
+  url.append('q', query);
+  url.append('limit', 20);
+  return await fetch(urlBase + url.toString(), {
     headers: {
+      Authorization: 'Bearer ' + token,
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
     },
   });
 };
@@ -37,30 +50,51 @@ window.onSpotifyWebPlaybackSDKReady = async () => {
       }
     });
     return valeur;
-  }
+  };
 
+  let current_device;
+  document.getElementById('togglePlay').hidden = true;
+  document.getElementById('searchbar').hidden = true;
   const token = getCookie('token');
   if (!token) return;
-  document.getElementById("login").hidden = true;
+  document.getElementById('login').hidden = true;
+  document.getElementById('togglePlay').hidden = false;
+  document.getElementById('searchbar').hidden = false;
 
   const player = new Spotify.Player({
     name: 'Listen Along',
-    getOAuthToken: cb => { cb(token); },
-    volume: 0.2
+    getOAuthToken: (cb) => {
+      cb(token);
+    },
+    volume: 0.2,
   });
 
   // Ready
   player.addListener('ready', async ({ device_id }) => {
-    await transferPlayback(device_id, token)
-    await play("spotify:track:3F4ujQNrH5qbEA8cAzxYUR", device_id, token)
-    await player.seek(0);
-    await player.togglePlay()
+    current_device = device_id;
+    await transferPlayback(device_id, token);
   });
 
   // Not Ready
-  player.addListener('not_ready', () => console.log("stopped"));
+  player.addListener('not_ready', () => console.log('stopped'));
 
-  document.getElementById('connectPlayer').onclick = () => player.connect();
-  document.getElementById('togglePlay').onclick = () => player.togglePlay();
-  document.getElementById('disconnectPlayer').onclick = () => player.disconnect();
-}
+  $('#togglePlay').click(() => player.togglePlay());
+
+  $('#searchbar-btn').click(() => {
+	if (document.getElementById('searchbar-val').value !== '') {
+		let query = document.getElementById('searchbar-val').value.trim().replaceAll(/\s+/g,'+');
+		searchItem(query, token)
+		  .then((response) => response.json())
+		  .then(async (json) => {
+			  let firstResult = json["tracks"]["items"][0];
+			  document.title = firstResult["name"] + " - Listen Along";
+			  $('meta[name=og\\:image]').attr('content', firstResult["album"]["images"][1]["url"] + ".jpg");
+			  $("#favicon").attr("href", firstResult["album"]["images"][2]["url"]);
+			  await play(firstResult["uri"], current_device, token);
+			  await player.seek(0);
+		  });
+	  }
+  });
+
+  player.connect();
+};
